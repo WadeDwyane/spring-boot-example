4ca71dab9aaa2105b5af3065d9feaa125aa7576b
kui.liu <kui01.liu@tempus.cn>
2018-12-26 11:24:06 +0800

    1.完善了工程

---
+++ springdatajpa-master/src/main/java/com/wadedwyane/www/SpringdatajpaApplication.java
@@ -0,0 +1,33 @@
+package com.wadedwyane.www;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableCaching
+/*开启定时任务*/
+//@EnableScheduling
+@ComponentScan({"com.wadedwyane.www"})
+@EntityScan("com.wadedwyane.www")
+@EnableJpaRepositories(value = "com.wadedwyane.www")
+@MapperScan("com.wadedwyane.www.mapper")
+public class SpringdatajpaApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdatajpaApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return  builder.sources(SpringdatajpaApplication.class);
+    }
+
+
+}
