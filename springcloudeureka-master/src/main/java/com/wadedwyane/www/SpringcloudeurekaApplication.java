779d7c6a042f827a640a4853476ce2cae283b921
kui.liu <kui01.liu@tempus.cn>
2018-12-26 11:35:23 +0800

    1.修改了文件启动文件

--- springcloudeureka-master/src/main/java/com/wadedwyane/www/SpringcloudeurekaApplication.java
+++ springcloudeureka-master/src/main/java/com/wadedwyane/www/SpringcloudeurekaApplication.java
@@ -2,17 +2,19 @@ package com.wadedwyane.www;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringcloudeurekaApplication {
+public class SpringcloudeurekaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudeurekaApplication.class, args);
     }
 
-
-
-
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringcloudeurekaApplication.class);
+    }
 }
 
