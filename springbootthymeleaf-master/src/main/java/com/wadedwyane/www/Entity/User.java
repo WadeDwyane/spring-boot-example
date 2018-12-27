bf1742274a56e25bacf04c5dd0f255631fa400fa
kui.liu <kui01.liu@tempus.cn>
2018-12-27 13:48:01 +0800

    1.完成了模板引擎thymeleaf的基础用法

---
+++ springbootthymeleaf-master/src/main/java/com/wadedwyane/www/Entity/User.java
@@ -0,0 +1,32 @@
+package com.wadedwyane.www.Entity;
+
+public class User {
+
+    private String name;
+    private int age;
+    private String signature;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+}
---
+++ springbootthymeleaf-master/src/main/java/com/wadedwyane/www/controller/ExampleController.java
@@ -0,0 +1,72 @@
+package com.wadedwyane.www.controller;
+
+import com.wadedwyane.www.Entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ExampleController {
+
+    @RequestMapping("/")
+    public String hello(ModelMap modelMap) {
+        modelMap.addAttribute("message", "www.wadedwyane.com");
+        return "hello";
+    }
+
+    @RequestMapping("/string")
+    public String string(ModelMap map) {
+        map.addAttribute("userName", "www.wadedwyane.com");
+        return "string";
+    }
+
+    @RequestMapping("/if")
+    public String ifUnless(ModelMap modelMap) {
+        modelMap.addAttribute("flag", "no");
+        return "if";
+    }
+
+    @RequestMapping("/list")
+    public String list(ModelMap map) {
+        map.addAttribute("users", getUserList());
+        return "list";
+    }
+
+    @RequestMapping("/url")
+    public String url(ModelMap map) {
+        map.addAttribute("type", "2018");
+        map.addAttribute("pageId", "2018/12/25/spring");
+        map.addAttribute("img", "http://t2.hddhhn.com/uploads/tu/201811/9999/b84485cb4d.jpg");
+        return "url";
+    }
+
+    @RequestMapping("/eq")
+    public String eq(ModelMap modelMap) {
+        modelMap.addAttribute("name", "wadedwyane");
+        modelMap.addAttribute("age", 31);
+        modelMap.addAttribute("flag", "yes");
+        return "eq";
+    }
+
+    @RequestMapping("/switch")
+    public String switchcase(ModelMap map) {
+        map.addAttribute("sex", "woman");
+        return "switch";
+    }
+
+    public List<User> getUserList() {
+        ArrayList<User> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+            user.setName("wade dwyane " + i);
+            user.setAge(20 + i);
+            user.setSignature("this is my house " + i);
+            list.add(user);
+        }
+        return list;
+    }
+
+}
